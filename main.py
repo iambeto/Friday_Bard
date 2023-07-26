7cb4f9d8e5136c5a9b23fe42d805f4371158e1f7
iambeto <rscabrera@gmail.com>
2023-07-26 12:27:57 -0500

    initial commit

--- main.py
+++ main.py
@@ -6,4 +6,38 @@ import whisper
 import warnings
 import sys
 
-token = ''
+token = 'PASTE YOUR TOKEN HERE'
+chatbot = Chatbot(token)
+r = sr.Recognizer()
+
+tiny_model = whisper.load_model('tiny')
+base_model = whisper.load_model('base')
+warnings.filterwarnings("ignore", message="FP16 is not support on CPU, using FP32 instead.")
+
+if sys.platform != 'darwin':
+    import pyttsx3
+    engine = pyttsx3.init()
+    rate = engine.getProperty('rate')
+    engine.setProperty('rate', rate-50)
+
+def prompt_bard(prompt):
+    response = chatbot.ask(prompt)
+    return response['content']
+
+def speak(text):
+    if sys.platform == 'darwin':
+        ALLOWED_CHARS = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,!?-_$: ")
+        cleaned_text = ''.join(c for c in text if c in ALLOWED_CHARS)
+        system(f'say "{cleaned_text}"')
+    else:
+        engine.say(text)
+        engine.runAndWait()
+
+def main():
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        while True:
+            while True:
+                try:
+                    print('\nSay "google" to wake me up. \n')
+                    audio = r.listen(source)
